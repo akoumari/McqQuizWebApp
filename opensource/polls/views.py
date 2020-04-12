49d9d5b46672a02c679885448f9aded69af41d04
akoumari <alex.koumarianos@gmail.com>
2020-04-12 19:58:18 -0400

    fixed front end some small cleanup remains but not today satan

--- opensource/polls/views.py
+++ opensource/polls/views.py
@@ -1,47 +1,63 @@
 from django.shortcuts import get_object_or_404, render
 
-from .models import Question, Choice
+from .models import Question, Choice, Test
 from django.http import Http404, HttpResponseRedirect
 from django.urls import reverse
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
+    latest_test_list = Test.objects.order_by('-pub_date')[:10]
+    context = {'latest_test_list': latest_test_list}
     return render(request, 'polls/index.html', context)
 
-def detail(request, question_id):
+def detail(request, test_id):
     try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question was not found...")
-    return render(request, 'polls/detail.html', {'question': question})
+        test = Test.objects.get(pk=test_id)
+    except Test.DoesNotExist:
+        raise Http404("Quiz was not found...")
+    return render(request, 'polls/detail.html', {'test': test})
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+def results(request, test_id ,score):
+    print(score)
+    test = get_object_or_404(Test, pk=test_id)
+    return render(request, 'polls/results.html', {'test': test, 'score': score})
     
-def vote(request, question_id):
+def vote(request, test_id):
     # print(request.POST['choice'])
-    question = get_object_or_404(Question, pk=question_id)
+    test = get_object_or_404(Test, pk=test_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        questions = Question.objects.filter(test=test.id)
+        selected_choice = []
+        for question in questions:
+            key = request.POST[str(question.id)]
+            selected_choice.append(test.choice_set.get(pk=key))
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
-            'question': question,
+            'test': test,
             'error_message': "You didn't select a choice.",
         })
     else:
-        selected_choice.votes += 1
-        question.total_votes += 1
-        selected_choice.save()
-        question.save()
-        choices = question.choice_set.all()
-        for choice in choices:
-            choice.vote_percent = ((choice.votes/question.total_votes)*100)
+        score = 0
+        print(selected_choice)
+        numQuestions = len(test.question_set.all())
+        for choice in selected_choice:
+            print(choice.question)
+            question = Question.objects.get(pk=choice.question.id)
+            choice.votes += 1
+            question.total_votes += 1
             choice.save()
-            
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+            question.save()
+            if choice.isCorrect == True:
+                print(choice.question)
+                score += 1
+            choices = question.choice_set.all()
+            for choice in choices:
+                choice.vote_percent = ((choice.votes/question.total_votes)*100)
+                choice.save()
+        print(score)
+        score = round((score/numQuestions)*100)
+                
+            # Always return an HttpResponseRedirect after successfully dealing
+            # with POST data. This prevents data from being posted twice if a
+            # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(test.id, score)))
